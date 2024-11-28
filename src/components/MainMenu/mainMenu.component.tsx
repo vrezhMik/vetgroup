f4bf2cae69feb537b0f57e1fefa244abc1c8df50
vrezhMik <mikayelyan.vrezh99@gmail.com>
2024-11-28 20:16:12 +0400

    add: main menu

--- src/components/MainMenu/mainMenu.component.tsx
+++ src/components/MainMenu/mainMenu.component.tsx
@@ -23,6 +23,12 @@ const data = [
 export default function MainMenu() {
   const [currentSubMenu, setCurrentSubMenu] = useState(-1);
   const showSubMenu = (id: number) => {
+    if (id == currentSubMenu) {
+      const nextSubClass = document.getElementById(`subclass_${id}`);
+      if (nextSubClass) nextSubClass.style.display = "none";
+      setCurrentSubMenu(-1);
+      return;
+    }
     const prevSubClass = document.getElementById(`subclass_${currentSubMenu}`);
     if (prevSubClass) prevSubClass.style.display = "none";
     const nextSubClass = document.getElementById(`subclass_${id}`);
@@ -35,7 +41,12 @@ export default function MainMenu() {
         <ul>
           {data.map((d, key) => (
             <li key={key}>
-              <button onClick={() => showSubMenu(key)}>{d.name}</button>
+              <button
+                onClick={() => showSubMenu(key)}
+                className={`${key == currentSubMenu ? style.active : ""}`}
+              >
+                {d.name}
+              </button>
               <ol id={`subclass_${key}`}>
                 {d.data.map((el, key) => (
                   <li key={`0_${key}`}>{el}</li>
