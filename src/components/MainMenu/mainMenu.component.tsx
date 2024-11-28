eca58f6d17c6210cc571b76a7fe68c4bbf3774b6
vrezhMik <mikayelyan.vrezh99@gmail.com>
2024-11-28 19:58:04 +0400

    add: sidebar

--- src/components/MainMenu/mainMenu.component.tsx
+++ src/components/MainMenu/mainMenu.component.tsx
@@ -1,16 +1,48 @@
+"use client";
 import style from "./mainMenu.module.scss";
+import { useState } from "react";
+const data = [
+  {
+    name: "Category",
+    data: ["Sub Category", "Sub Category", "Sub Category", "Sub Category"],
+  },
+  {
+    name: "Category1",
+    data: ["Sub Category1", "Sub Category", "Sub Category", "Sub Category"],
+  },
+  {
+    name: "Category2",
+    data: ["Sub Category2", "Sub Category", "Sub Category", "Sub Category"],
+  },
+  {
+    name: "Category3",
+    data: ["Sub Category3", "Sub Category", "Sub Category", "Sub Category"],
+  },
+];
 
-import Link from "next/link";
 export default function MainMenu() {
+  const [currentSubMenu, setCurrentSubMenu] = useState(-1);
+  const showSubMenu = (id: number) => {
+    const prevSubClass = document.getElementById(`subclass_${currentSubMenu}`);
+    if (prevSubClass) prevSubClass.style.display = "none";
+    const nextSubClass = document.getElementById(`subclass_${id}`);
+    if (nextSubClass) nextSubClass.style.display = "block";
+    setCurrentSubMenu(id);
+  };
   return (
     <div className={style.mainMenu}>
       <nav>
         <ul>
-          <li>Category</li>
-          <li>Category</li>
-          <li>Category</li>
-          <li>Category</li>
-          <li>Category</li>
+          {data.map((d, key) => (
+            <li key={key}>
+              <button onClick={() => showSubMenu(key)}>{d.name}</button>
+              <ol id={`subclass_${key}`}>
+                {d.data.map((el, key) => (
+                  <li key={`0_${key}`}>{el}</li>
+                ))}
+              </ol>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
