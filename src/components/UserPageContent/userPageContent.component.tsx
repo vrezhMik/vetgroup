c920622f7c2e00d2160662f11fa2335cb09db483
vrezhMikTumo <mikayelyan.vrezh99@gmail.com>
2024-12-09 23:45:23 +0400

    add: password

--- src/components/UserPageContent/userPageContent.component.tsx
+++ src/components/UserPageContent/userPageContent.component.tsx
@@ -1,9 +1,11 @@
+import Password from "../Password/password.component";
 import UserPageMenu from "../UserPageMenu/userPageMenu.component";
 import style from "./userPageContent.module.scss";
 export default function UserPageContent() {
   return (
     <div className={style.content}>
       <UserPageMenu />
+      <Password/>
     </div>
   );
 }
