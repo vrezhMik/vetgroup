b4d9416a60f47a7873a0b3561a816186531b256f
vrezhMik <mikayelyan.vrezh99@gmail.com>
2025-06-02 01:42:09 +0400

    bug: fix

--- src/components/UserComponents/UserMenu/userMenu.component.tsx
+++ src/components/UserComponents/UserMenu/userMenu.component.tsx
@@ -65,7 +65,7 @@ export default function UserMenu() {
   };
 
   const cleanFilters = () => {
-    const store = productsStore.getState();
+    // const store = productsStore.getState();
     productsStore.getState().resetSelectedCategories();
     // store.setLoading(true);
     router.push("/");
