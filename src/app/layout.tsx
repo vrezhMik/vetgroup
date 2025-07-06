169b53efe4459e5e3fe5dbef52b630e9d26f816b
vrezhMikTumo <vrezh.mikauyelyan@tumo.org>
2025-07-06 17:50:40 +0400

    udpate: style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.scss";
 import type { Metadata } from "next";
 import Card from "@/components/CardComponents/Card/card.component";
 import { Montserrat } from "next/font/google";
+import Head from "next/head";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -18,6 +19,12 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        ></meta>
+      </Head>
       <body className={montserrat.className}>
         {children}
         <Card />
